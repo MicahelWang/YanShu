a1186feab4ffb5fe487c81e366fbce88dc2ac578
Michael <490811331@qq.com>
2016-05-09 18:19:50 +0800

    Update

---
+++ WeChatPortal/Areas/Mobile/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChatPortal.Areas.Mobile.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Mobile/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WeChatPortal/Areas/Mobile/Controllers/PlanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChatPortal.Areas.Mobile.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: Mobile/Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
