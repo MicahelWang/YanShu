c8621a8dca87d9518cbed9e7b91c372d21b72246
Michael <490811331@qq.com>
2016-05-13 02:27:12 +0800

    Update

--- WeChatPortal/Filters/ApiActionFilterAttribute.cs
+++ WeChatPortal/Filters/ApiActionFilterAttribute.cs
@@ -28,9 +28,8 @@ namespace WeChatPortal.Filters
             base.OnActionExecuting(actionContext);
             var request = actionContext.Request;
             string url = request.RequestUri.AbsoluteUri;
-            string queryString = request.RequestUri.Query;
-            string requestFormat = "Request Url：\r\n{0},Query;{1}";
-            Log4NetHelper.WriteLog(string.Format(requestFormat, url, queryString));
+            string requestFormat = "Request Url：\r\n{0}";
+            Log4NetHelper.WriteLog(string.Format(requestFormat, url));
         }
 
 
