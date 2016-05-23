f3b8946e3d00658ca4bee2be43439f5427dde799
Michael <490811331@qq.com>
2016-05-23 09:40:15 +0800

    Update

--- WeChatPortal/Utils/ConfigSetting.cs
+++ WeChatPortal/Utils/ConfigSetting.cs
@@ -107,19 +107,25 @@ namespace WeChatPortal.Utils
         private const string EncodingAesKeyName = "EncodingAesKey";
         private const string EncryptKey = "Encrypt";
         private const string HostUrlKey = "HostUrl";
+        private const string CodeHashKey = "CodeHash";
 
         #region Public Properties
 
         public static string AppId => GetString(AppIdKey, "");
-        public static string HostUrl => GetString(HostUrlKey, "");
 
         public static string AppSecret => GetString(AppSecretKey, "");
         public static string Token => GetString(TokenKey, "");
 
-        public static string EncodingAesKey => GetString(EncodingAesKeyName, "");/// <summary>
-                                                                                 /// 是否加密
-                                                                                 /// </summary>
+        public static string EncodingAesKey => GetString(EncodingAesKeyName, "");
+
+        /// <summary>
+        /// 是否加密
+        /// </summary>
         public static bool Encrypt => GetBool(EncryptKey, false);
+
+
+        public static string HostUrl => GetString(HostUrlKey, "");
+        public static string CodeHash => GetString(CodeHashKey, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
         #endregion
     }
--- WeChatPortal/Utils/Helper.cs
+++ WeChatPortal/Utils/Helper.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace WeChatPortal.Utils
 {
     public static class Helper
     {
+        private static readonly string[] Codes;
+        private static readonly int CodeLength;
+        private static readonly string StartChar;
+        static Helper()
+        {
+            Codes = ConfigSetting.CodeHash.ToCharArray().Select(m => m.ToString()).ToArray();
+            CodeLength = Codes.Length;
+            StartChar = Codes[0];
+        }
         /// <summary>
         /// 验证微信签名
         /// </summary>
@@ -32,5 +42,32 @@ namespace WeChatPortal.Utils
             byte[] hashedBytes = System.Security.Cryptography.SHA1.Create().ComputeHash(cleanBytes);
             return BitConverter.ToString(hashedBytes).Replace("-", "");
         }
+
+        public static string ToCodeValue(string code)
+        {
+            return "";
+        }
+
+        public static string CreateCode(this int value)
+        {
+            string[] result = new string[6];
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (value > 0)
+                {
+                    result[i] = Codes[value % CodeLength];
+                    value = value / CodeLength;
+                }
+                else
+                {
+                    result[i] = StartChar;
+                }
+            }
+            return string.Join("", result);
+
+        }
+
+
+
     }
 }
